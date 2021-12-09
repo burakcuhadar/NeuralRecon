f2573bc6e550031939d8f84553e780857d18252e
burakcuhadar <burakcuhadar96@gmail.com>
2021-12-09 09:22:20 +0100

    Update train.sh to adapt it for our machine

--- train.sh
+++ train.sh
@@ -1,3 +1,4 @@
 #!/usr/bin/env bash
-export CUDA_VISIBLE_DEVICES=0,1
-python -m torch.distributed.launch --nproc_per_node=2 main.py --cfg ./config/train.yaml
+export CUDA_VISIBLE_DEVICES=2
+python main.py --cfg ./config/train.yaml MODEL.FUSION.FUSION_ON False MODEL.FUSION.FULL False TRAIN.EPOCHS 20
+
