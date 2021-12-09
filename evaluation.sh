e576aa2571dbf229a1b8a9e26ce539d4c32b1cb8
burakcuhadar <burakcuhadar96@gmail.com>
2021-12-09 09:57:36 +0100

    Remove old scripts

--- evaluation.sh
+++
@@ -1,6 +0,0 @@
-#!/usr/bin/env bash
-PYOPENGL_PLATFORM=osmesa python tools/evaluation.py \
-	--model ./results/scene_scannet_checkpoints_fusion_eval_47 \
-	--n_proc 2 --n_gpu 1 --num_workers=1 --loader_num_workers=2 \
-	--data_path=/mnt/raid/dareth/ScanNet/scannet/scans_test \
-	--gt_path=/mnt/ScanNet/public/v2/scans
